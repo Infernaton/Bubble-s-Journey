465e1d3466cc33d1353859e2438b8c846c3e34ae
Infernaton <lubin.pauly@gmail.com>
2024-10-31 13:20:25 +0100

    Spawn bubble at start of the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,33 @@
 using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEditor;
 
 public class GameManager : MonoBehaviour
 {
     [Header("Init Game")]
     [SerializeField] float m_NbStartBubble;
+    [SerializeField] float m_SpawnTime;
     [SerializeField] Bubble m_BubblePrefab;
+    [SerializeField] Vector3 m_SpawnPos1;
+    [SerializeField] Vector3 m_SpawnPos2;
 
     [Header("During Game")]
     [SerializeField] Camera m_Camera;
     [SerializeField] GameObject m_Background;
     [SerializeField] Wind m_WindPrefab;
 
+    [SerializeField] float m_CameraStartScroll;
     [SerializeField] float m_ScrollingSpeed;
 
     private Vector3 _startWind;
     private Vector3 _endWind;
+
+    private void Start()
+    {
+        float time = m_SpawnTime / m_NbStartBubble;
+        InvokeRepeating(nameof(SpawnBubble), 0, time);
+        Invoke(nameof(CancelInvoke), m_SpawnTime);
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,7 +42,15 @@ public class GameManager : MonoBehaviour
             w.Init(_startWind, _endWind);
         }
 
+        if (Time.time - m_SpawnTime - m_CameraStartScroll < 0) return;
+
         m_Camera.transform.position += Vector3.up * m_ScrollingSpeed * Time.deltaTime;
+    }
+
+    void SpawnBubble()
+    {
+        Vector3 pos = Vector3.Lerp(m_SpawnPos1, m_SpawnPos2, Random.value);
+        Instantiate(m_BubblePrefab, pos, Quaternion.identity, transform);
     }
 
     Vector3 GetMousePosition()
@@ -45,5 +65,11 @@ public class GameManager : MonoBehaviour
         Physics.Raycast(rayCast, out RaycastHit hit, Camera.main.farClipPlane);
         Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red);
         return new Vector3(hit.point.x, hit.point.y, 0);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(m_SpawnPos1, m_SpawnPos2);
     }
 }
